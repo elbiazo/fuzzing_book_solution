5320266c62ce6c33c3bb904ba1dbb3270aa66499
ebiazo <ebiazo@researchinnovations.com>
2021-12-09 22:51:49 -0500

    ading file

--- lexical/src/main.rs
+++ lexical/src/main.rs
@@ -1,16 +1,27 @@
 use fuzzingbook::SimpleFuzzer;
+use std::fs::File;
+use std::io;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::ops::RangeInclusive;
+use std::str;
 use tempfile::tempfile;
-use std::io::{self, Write, Read};
 
 fn main() -> Result<(), io::Error> {
-    let sf = SimpleFuzzer::new(100, RangeInclusive::new(255, 255));
-    println!("{:?}", sf.fuzz());
+    let sf = SimpleFuzzer::new(100, RangeInclusive::new('A' as u8, 'Z' as u8));
+    // println!("{:?}", str::from_utf8(&sf.fuzz()).unwrap());
 
-    let mut file = tempfile()?;
-    writeln!(file, "Hello, world!")?;
-    println!("{:?}", file);
+    // Write
+    let mut tmpfile: File = tempfile()?;
+    // write!(tmpfile, "Hello World!")?;
+    tmpfile.write_all(str::from_utf8(&sf.fuzz()).unwrap().as_bytes());
 
+    // Seek to start
+    tmpfile.seek(SeekFrom::Start(0)).unwrap();
+
+    // Read
+    let mut buf = Vec::new();
+    tmpfile.read_to_end(&mut buf)?;
+    println!("{:?}", str::from_utf8(&buf).unwrap());
 
     Ok(())
 }
